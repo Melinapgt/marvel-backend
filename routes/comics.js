417c6c5f1f8d6516efa78fd6a875e1b44bd27479
Melinapgt <poignetmelina@gmail.com>
2022-02-17 16:53:58 +0100

    filter comics

--- routes/comics.js
+++ routes/comics.js
@@ -8,16 +8,21 @@ const apikey = process.env.MARVEL_APIKEY;
 router.get("/comics", async (req, res) => {
   try {
     console.log(req.query);
+    console.log(req.query.title);
+
+    //gestion des query pour la recherche soit title soit name
+    const filter = {};
+    if (req.query.title) {
+      filter.title = new RegExp(req.query.title, "i");
+    }
 
     const response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apikey}`
+      // `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apikey}?title=${filter}`
     );
-    console.log(response.data);
+    // console.log(response.data);
+
     res.status(200).json(response.data);
-
-    // const results = response.data.results;
-
-    res.json(results);
   } catch (error) {
     console.log("error.response ==>", error.response);
     res.status(400).json({ error: "Bad request" });
